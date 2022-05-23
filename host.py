d1c6b6791874028ba9d11eebd948cece1f642e21
Luis Hernandez <38053801+Heernandez@users.noreply.github.com>
2022-05-23 00:57:25 -0500

    Host complete, save in db and send bot msg

--- host.py
+++ host.py
@@ -2,38 +2,54 @@ import mysql.connector
 import json
 import paho.mqtt.publish as publicar
 import paho.mqtt.client as mqtt
+import os
+from datetime import datetime
+import sqlite3
+
 
 class Host:
 
     def __init__(self):
-        self.mydb = mysql.connector.connect(host="localhost",user="luish",password="luis123",database="Especializacion")
+        self.id  = "1573972812"
+        self.tkn = "5347196612:AAFZVEr7SNfJT06guh12HoYlMUza4QSSfDA"
+        #msg = "Hola IOT"
+        self.mydb = sqlite3.connect('Especializacion.db')
+        
+        #self.mydb = mysql.connector.connect(host="localhost",user="luish",password="luis123",database="Especializacion")
      
 
     def ejecutar_query(self,payload):
         mycursor = self.mydb.cursor()
         sql = "select * from ESP32A"
+        fecha = "'"+datetime.today().strftime('%Y-%m-%d %H:%M:%S')+"'"
         if(payload["ESP"] == "ESP32A"):
-            sql = "INSERT INTO esp32a " \
-                  "VALUES (current_timestamp(),{},{});".format(
-                payload["Temperatura"], payload["Humedad"])
-
+            sql = "INSERT INTO esp32a VALUES ({},{},{});".format(fecha,payload["Temperatura"], payload["Humedad"])
+            self.notificar(payload["Temperatura"],payload["Humedad"])
         if(payload["ESP"] == "ESP32B"):
-            sql = "INSERT INTO esp32b " \
-                  "VALUES (current_timestamp(),{});".format(payload["NuevoEstado"])
+            sql = "INSERT INTO esp32b VALUES ({},{});".format(fecha,payload["NuevoEstado"])
         try:
             mycursor.execute(sql)
-            self.mydb.commit()
-            #self.mydb.close()
+            mycursor.execute("commit;")
+            mycursor.close()
         except Exception as e:
-            print("Formato incorrecto \n", e)
 
+            print("\nFormato incorrecto \n{}\n{}".format(sql,e))
+
+    def notificar(self,T=98,H=98):
+        #msg = "{'Temperatura':'92','Humedad':'10'}"
+        msg = "Temperatura: {}%0AHumedad: {}".format(T,H)
+        msg = msg.replace(" ","%20")
+        #completeMsg = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(self.tkn,self.id,msg)
+        #command = 'curl {}'.format(completeMsg)
+        command2 = "curl -G -d 'chat_id={}' -d 'text={}' https://api.telegram.org/bot{}/sendMessage".format(self.id,msg,self.tkn)
+        #print(command2)
+        os.system(command2)
 
 
 def on_connect(client, userdata, flags, rc):
 
     # Topico al que desea suscribirse
     client.subscribe("CargaData")
-
 
 def on_message(client, userdata, msg):
     # Operacion a realizar cuando llegue un mensaje
@@ -46,8 +62,8 @@ def on_message(client, userdata, msg):
     except Exception as e:
         print("Falla ", e)
 
-
 h = Host()
+
 
 # Creo un cliente o creo un objeto de tipo cliente
 cliente = mqtt.Client()
